3478f3bcb8c9f124341cad4ee2e1a1039cb76ed8
nevil-ctrl <romsonsonrem@gmail.com>
2025-05-16 10:18:07 +0600

    Add files via upload

---
+++ Exam_future_academy/pages/index.php
@@ -0,0 +1,13 @@
+<?php $titleName = "Главная"; ?>
+<?php include ('../layout/header.php'); ?>
+
+
+<main>
+  <?php include ('../layout/nav.php'); ?> 
+  <?php include ('../include/banner.php'); ?>
+  <?php include ('../include/about_webdev.php'); ?>
+
+</main>
+
+<?php include ('../layout/footer.php'); ?>
+<script src="../assets\js\app.js"></script>
